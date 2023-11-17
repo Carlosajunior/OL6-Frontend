a64b389c07a245579b807ed9cd0fd6e39063aecd
Carlosajunior <carlosajunior@hotmail.com>
2023-11-16 22:24:26 -0300

    feat: estilizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
+import { useState } from "react";
 import "./styles.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="container">
       <div className="container-login">
         <div className="wrap-login">
           <form className="login-form">
-            <span className="login-form-title">OL6</span>
+            <h1 className="login-form-title">OL6</h1>
             <div className="wrap-input-email">
-              <input className="input-email" type="email" />
+              <input
+                className={
+                  email !== "" ? "has-value input-email" : "input-email"
+                }
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
               <span className="focus-input" data-placeholder="Email"></span>
             </div>
+
             <div className="wrap-input-password">
-              <input className="input-password" type="password" />
+              <input
+                className={
+                  password !== ""
+                    ? "has-value input-password"
+                    : "input-password"
+                }
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <span className="focus-input" data-placeholder="Senha"></span>
             </div>
+
             <div className="container-login-form-btn">
               <button className="login-form-btn">Login</button>
             </div>
+
             <div className="text-center-login-form">
               <span className="text-for-non-registered-user">
                 Não possui conta?
